a14363b0dc5140af5024b77a310ba947c3d84cb2
Katrina Hockman and Scott Bergler <scott@>
2019-01-08 16:45:22 -0800

    add everything.

---
+++ src/TriangleNodeJS.js
@@ -0,0 +1,19 @@
+export function Triangle(side1, side2, side3) {
+  this.side1 = side1;
+  this.side2 = side2;
+  this.side3 = side3;
+}
+
+Triangle.prototype.checkType = function() {
+
+  if (this.side1 === this.side2 && this.side1 === this.side3) {
+    return 'equilateral';
+  } else if (this.side1 + this.side2 <= this.side3 || this.side2 + this.side3 <= this.side1 || this.side1 + this.side3 <= this.side2) {
+    return "not a triangle";
+  } else if (this.side1 === this.side2 || this.side2 === this.side3 || this.side3 === this.side1) {
+  return 'isosceles';
+  } else if (this.side1 !== this.side2 && this.side1 !== this.side3 && this.side2 !== this.side3) {
+  return 'scalene';
+  }
+
+}
